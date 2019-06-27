3a7de7b504f6edfeff8bb6f60829b67b2491e072
Gustavo Moraes <gustavo.moraes@sovos.com>
2019-06-27 08:13:30 -0300

    novo teste botao

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component, Fragment } from 'react';
-import Body from '../src/components/Body';
-import './App.css';
+import React, {Component} from "react"
+import Button from "./component/Button.js"
 
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Body />
-      </Fragment>
-    )
+  state= {
+    valor1: "teste 1",
+    valor2: "teste 2"
   }
+
+  ok =()=>{
+    if (!this.valor1)  return <Button w={this.setState({valor1: "valor2"})} />
+  }
+
+  render() {
+  return (
+    <div onClick={()=>this.ok()}>
+      <button>{this.state.valor1}</button>
+    </div>
+  );
+}
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Head from './Head';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Head />, document.getElementById('header'));
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
